49c64aa1eddfb5ac09ebb228d9128c583155d81b
Uthaeus <romanlavery@gmail.com>
2018-03-28 12:22:02 -0600

    Introduction to promises

---
+++ 12_intro_to_promises.js
@@ -0,0 +1,21 @@
+// Twitter > Posts
+// if twitter was down user would just see buffering
+
+
+let sleepyGreeting = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('Hello...')
+  }, 2000);
+
+  setTimeout(() => {
+    reject(Error('Too sleepy...'))
+  }, 2000);
+});
+
+sleepyGreeting
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.error(err);
+  });
