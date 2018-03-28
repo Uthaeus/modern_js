e35039db97bc0f8728a19ae7a9b6854479018b0e
Uthaeus <romanlavery@gmail.com>
2018-03-28 14:20:53 -0600

    Combining async and await with closures

---
+++ 16_async_and_await_with_closures.js
@@ -0,0 +1,29 @@
+// Closures
+
+const login = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('User logged in....');
+    }, 2000);
+  });
+}
+
+const updateAccount = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Updating last login...');
+    }, 2000);
+  });
+}
+
+async function loginActivities(login, updateAccount) {
+  const returnedLogin = await login;
+  console.log(returnedLogin);
+    // => User logged in...
+
+  const returnedUpdateAccount = await updateAccount;
+  console.log(returnedUpdateAccount);
+    // => Updating last login
+}
+
+loginActivities(login(), updateAccount());
